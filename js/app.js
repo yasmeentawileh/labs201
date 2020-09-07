5dc862910695752bb74876fe80e9e9a4ac669967
yasmeentawileh <yasmeen.tawileh@gmail.com>
2020-09-07 17:27:22 +0300

    lab2

---
+++ js/app.js
@@ -0,0 +1,50 @@
+'use strict';
+var userName =prompt('what is you name?');
+alert('greeting Mr/Mrs :' + userName);
+confirm('let\'s make a quick quiz please answer with yes,no or y,n');
+var year = prompt('Did i graduate in 2019?'); year.toLowerCase;
+if(year === 'y'|| year === 'yes') {
+  alert('correct i graduated in 2019');
+  console.log('correct i graduated in 2019');
+}else{
+  alert('wrong i graduated in 2019');
+  console.log('wrong i graduated in 2019');
+}
+var study = prompt('Am i a doctor?');study.toLowerCase;
+if(study ==='n'||study==='no'){
+  alert('correct i am not a doctor');
+  console.log('correct i am not a doctor');
+}else{
+  alert('wrong i am an engineer');
+  console.log('wrong i am an engineer');
+}
+var age =prompt('am i 24 years?');age.toLowerCase;
+if(age ==='yes' || age ==='y'){
+  alert('correct i am 24 years old');
+  console.log('correct i am 24 years old');
+}else{
+  alert('wrong,i am 24 years old');
+  console.log('wrong,i am 24 years old');
+}
+var excel= prompt('do i have an excel certificate ?');excel.toLowerCase;
+if(excel ==='no'||excel==='n'){
+  alert('wrong i have an excel certificate');
+  console.log('wrong i have an excel certificate');
+}else{
+  alert('correct i have excel certificate');
+  console.log('correct i have excel certificate');
+}
+var jordan= prompt('am i jordanion?');jordan.toLowerCase;
+if(jordan ==='n'||jordan==='n'){
+  alert('wrong i am jordanion');
+  console.log('wrong i am jordanion');
+}else{
+  alert('correct i am jordanion');
+  console.log('correct i am jordanion');
+}
+alert('Welcome Mr/Mrs' + name + ' to my website i think now you know me more');
+
+
+
+
+
